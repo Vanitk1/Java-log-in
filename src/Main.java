728e9c2cc37cfd9e52c703262fe6f187f4879bb3
Vanit Kashab <vanitk1@gmail.com>
2026-03-23 18:13:05 +0000

    log in logic updated

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
 
         String user = "user";
-        String userPassword = "moon";
+        String userPassword = "Password";
 
         byte attempts = 0;
 
@@ -24,13 +24,48 @@ public class Main {
             IO.println("Please enter your password: ");
             String password = reader.nextLine();
 
+            if (userName.isEmpty()) {
+                IO.println("username is empty!");
+                attempts++;
+                continue;
+            }
+
+            if (password.isEmpty()) {
+                IO.println("password is empty!");
+                attempts++;
+                continue;
+            }
+
+            if (userName.length() < 2 ) {
+                IO.println("username must be at least 2 characters!");
+                attempts++;
+                continue;
+            }
+
+            if (password.length() < 2) {
+                IO.println("password must be at least 2 characters!");
+                attempts++;
+                continue;
+            }
+
+            boolean hasCapitalLetter = false;
+
+            for (int i = 0; i < password.length(); i++) {
+                if (Character.isUpperCase(password.charAt(i))) {
+                    hasCapitalLetter = true;
+                }
+            }
+
+            if (!hasCapitalLetter) {
+                IO.println("password must contain one uppercase letter!");
+                attempts++;
+                continue;
+            }
+
+
             if (userName.equals(user) && password.equals(userPassword)) {
                 IO.println("login successful");
                 return; // loop continues without this!!!!
-
-            } else if (userName.equals("") || password.equals("")) { // I didn't use .equals() before I was using == .equals() for strings
-                IO.println("username or password is empty! :o Please try again");
-                attempts++;
 
             } else {
                 IO.println("login-failed, Please enter your username and password again :(");
